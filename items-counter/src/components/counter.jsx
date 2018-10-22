c791ab9cd9babb58af500594ade0d6d36c9b7d96
Fakorede Oluwakunle <fakoredeoluwakunle2626@gmail.com>
2018-10-22 13:45:06 +0100

    (feature) Adds new counters component

--- items-counter/src/components/counter.jsx
+++ items-counter/src/components/counter.jsx
@@ -1,27 +1,47 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-class Counter extends Component {
-  state = {  
-    count: 0,
-    tags: ['Onions', 'Tomatoes', 'Fish']
+class Value extends Component {
+  state = {
+    value: this.props.counter.value,
+    id: this.props.id
   };
 
-  renderTags() {
-    if (this.state.tags.length === 0) {
-      return <p>There are no tags, fool!</p>
-    }
-    return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>
-  }
-  
-  render() { 
+  handleIncrement = () => {
+    this.setState({ value: this.state.value + 1 });
+  };
 
+  /** Whenever you need to pass an argument to an even handler, you use a helper method like
+  doHandleIncrement = () => {
+    this.handleIncrement({id:1})
+  }
+  and you refrence the doHandleIncrement function.
+  Or you simple pass an arrow function to the onClick attribute and call the event handler
+  */
+  render() {
     return (
-      <React.Fragment>
-        {this.state.tags.length === 0 && "Please create a new tag"}
-        {this.renderTags()}
-      </React.Fragment>
+      <div>
+        <span className={this.getBadgeClasses()}>{this.formatValue()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        <button onClick={() => this.props.onDelete(this.props.counter.id)} className="btn btn-danger btn-sm m-2">Delete</button>
+      </div>
     );
   }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.value === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatValue() {
+    const { value } = this.state;
+    return value === 0 ? "Zero" : value;
+  }
 }
- 
-export default Counter;
+
+export default Value;
