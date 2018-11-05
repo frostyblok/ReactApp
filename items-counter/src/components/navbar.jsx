8148dd8583224779070516281d697fba0ef34e81
Fakorede Oluwakunle <fakoredeoluwakunle2626@gmail.com>
2018-11-05 23:42:16 +0100

    (feature) Removes local state

--- items-counter/src/components/navbar.jsx
+++ items-counter/src/components/navbar.jsx
@@ -1,12 +1,18 @@
 import React, { Component } from "react";
 
 class NavBar extends Component {
+  getBadge() {
+    let classes = "badge badge-pill badge-secondary badge-";
+    classes += this.props.totalItem === 0 ? "primary" : "warning";
+    return classes;
+  }
+
   render() {
     return (
       <nav className="navbar navbar-light bg-light">
         <a className="navbar-brand" href="#">
-          ItemCounter{" "}
-          <span className="badge badge-pill badge-secondary">
+          ItemCounter
+          <span className={this.getBadge()}>
             {this.props.totalItem}
           </span>
         </a>
